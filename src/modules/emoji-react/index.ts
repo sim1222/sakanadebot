798ba2699aca7cac6dd84ea054aa71545f39aacb
sim1222 <kousuke11236124@gmail.com>
2021-12-22 02:38:29 +0900

    ～ていいに対してだめ

--- src/modules/emoji-react/index.ts
+++ src/modules/emoji-react/index.ts
@@ -73,6 +73,7 @@ export default class extends Module {
 		if (includes(note.text, ['おはよう', '起きた', '起きた', 'おきた', 'おきる'])) return react(':neyoune:');
 		if (includes(note.text, ['おやすみ', 'ねる', '寝る', 'ねむたい', 'ねむい'])) return react(':dame:');
 		if (includes(note.text, ['逆なでBot', '逆なで', '逆にゃでBot', '逆にゃで'])) return react(':ha_q:');
+		if (includes(note.text, ['ていい?', 'ていい？',])) return react(':dame:');
 
 		if (includes(note.text, ['寿司', 'sushi']) || note.text === 'すし') return react('🍣');
 	}
